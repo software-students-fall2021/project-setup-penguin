745c9ca4a2a632e4658b146979ef5a6fc57b1b5e
Janet <jh6248@nyu.edu>
2021-10-21 21:11:40 -0400

    implement basic common styling (navbar & container

--- front-end/src/views/signup/Signup.jsx
+++ front-end/src/views/signup/Signup.jsx
@@ -3,24 +3,26 @@ import "./Signup.css";
 function Signup() {
   return (
     <div className="Signup">
-      <div className="Signup__header">Sign up</div>
-      <input className="Signup__input" placeholder="Name"></input>
-      <input className="Signup__input" placeholder="Email"></input>
-      <input className="Signup__input" placeholder="Password"></input>
-      <div className="Signup__button"> sign up</div>
-      <div className="Signup__text">
-        or log in{" "}
-        <a className="Login__link " href="/login">
-          here
-        </a>
-      </div>
-      <div className="Signup__faqHeader">Why sign up?</div>
+      <h1>Sign up</h1>
+      <div className="Signup__body">
+        <input className="Signup__input" placeholder="Name"></input>
+        <input className="Signup__input" placeholder="Email"></input>
+        <input className="Signup__input" placeholder="Password"></input>
+        <div className="Signup__button"> sign up</div>
+        <div className="Signup__text">
+          or log in{" "}
+          <a className="Login__link " href="/login">
+            here
+          </a>
+        </div>
+        <div className="Signup__faqHeader">Why sign up?</div>
 
-      <div className="Signup__faqText">
-        1. Update your decks & cards anytime
-      </div>
-      <div className="Signup__faqText">
-        2. Get notified when others join your team
+        <div className="Signup__faqText">
+          1. Update your decks & cards anytime
+        </div>
+        <div className="Signup__faqText">
+          2. Get notified when others join your team
+        </div>
       </div>
     </div>
   );
