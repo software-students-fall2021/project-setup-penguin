3511bcd0b881f0462e029cdad88a149f30affa33
cindykimxp <cindykim100@gmail.com>
2021-10-23 21:25:07 -0400

    basic displayCard done and basic deckview, need more css detailing
    - finished with displayCard: a component that receives an input array with individualized values, and displays Card accordingly (1 at a time.)
    - basic deckview is finished --> will show up to 3 cards in a row.
    - card size made smaller for better deck view (see more at a time)
    - more css detailing, but basic functionality finished.
    - related to issue #31 and issue #25

--- front-end/src/common/DisplayCard.jsx
+++ front-end/src/common/DisplayCard.jsx
@@ -1,1 +1,73 @@
-// DisplayCard.jsx
+import "./card-editor/CardEditor.css";
+import "./DisplayCard.css";
+import heart from "../assets/heart.png";
+import kev from "../assets/kev.jpeg";
+
+//DisplayCard accepts an array of values as input and displays the card accordingly
+function DisplayCard(props) {
+  const sectionIds = [0, 1, 2];
+  const HeartIcon = () => <img src={heart} width="25px" height="25px" />;
+  const displayArray = props.tempArray;
+
+  // console.log("hello", props.tempArray.name);
+  // let temp = props.tempArray.name;
+  return (
+    <div className="CardEditor">
+      <div className="CardEditor__form" id="myCard">
+        <div className="CardEditor__upperContent">
+          <input
+            className="CardEditor__tagline"
+            type="text"
+            name="tagline"
+            value={displayArray.tagline}
+          />
+          <div className="CardEditor__header">
+            <input
+              type="text"
+              className="CardEditor__name"
+              name="name"
+              value={displayArray.name}
+            />
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <input
+                className="CardEditor__city"
+                type="text"
+                name="city"
+                value={displayArray.city}
+                size="5"
+              />
+              <HeartIcon />
+            </div>
+          </div>
+          <img className="CardEditor__image" src={kev} />
+        </div>
+        <input
+          className="CardEditor__summary"
+          type="text"
+          name="summary"
+          value={displayArray.summary}
+        />
+        {sectionIds.map((id) => (
+          <div className="CardEditor__section">
+            <HeartIcon />
+            <div className="CardEditor__sectionContent">
+              <input
+                type="text"
+                className="CardEditor__label"
+                name={`sectionLabel${id}`}
+                value={displayArray[`sectionLabel${id}`]}
+              />
+              <textarea
+                rows="2"
+                name={`sectionContent${id}`}
+                value={displayArray[`sectionContent${id}`]}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default DisplayCard;
--- front-end/src/views/DeckView.jsx
+++ front-end/src/views/DeckView.jsx
@@ -1,13 +1,101 @@
 import { useParams, NavLink } from "react-router-dom";
+import DisplayCard from "../common/DisplayCard";
+import "./DeckView.css";
 
 function DeckView() {
   let { id } = useParams();
   console.log({ id });
 
+  const TempOne = {
+    name: "Bob Ross",
+    city: "NYC",
+    tagline: "I love to paint, and I love to laugh.",
+    summary: "Designer (# YOE), 9AM-5PM EST",
+    sectionLabel0: "Strengths",
+    sectionLabel1: "Weaknesses",
+    sectionLabel2: "Communication Preferences",
+    sectionContent0: "Making happy mistakes",
+    sectionContent1: "Nothing",
+    sectionContent2: "bobross@happymistakes.com",
+    sliderLabelMin: "Introvert",
+    sliderLabelMax: "Extrovert",
+    sliderValue: 80,
+  };
+
+  const TempTwo = {
+    name: "Bob Ross",
+    city: "NYC",
+    tagline: "I love to paint, and I love to laugh.",
+    summary: "Designer (# YOE), 9AM-5PM EST",
+    sectionLabel0: "Strengths",
+    sectionLabel1: "Weaknesses",
+    sectionLabel2: "Communication Preferences",
+    sectionContent0: "Making happy mistakes",
+    sectionContent1: "Nothing",
+    sectionContent2: "bobross@happymistakes.com",
+    sliderLabelMin: "Introvert",
+    sliderLabelMax: "Extrovert",
+    sliderValue: 80,
+  };
+
+  const TempThree = {
+    name: "Bob Ross",
+    city: "NYC",
+    tagline: "I love to paint, and I love to laugh.",
+    summary: "Designer (# YOE), 9AM-5PM EST",
+    sectionLabel0: "Strengths",
+    sectionLabel1: "Weaknesses",
+    sectionLabel2: "Communication Preferences",
+    sectionContent0: "Making happy mistakes",
+    sectionContent1: "Nothing",
+    sectionContent2: "bobross@happymistakes.com",
+    sliderLabelMin: "Introvert",
+    sliderLabelMax: "Extrovert",
+    sliderValue: 80,
+  };
+
+  const TempFour= {
+    name: "Bob Ross",
+    city: "NYC",
+    tagline: "I love to paint, and I love to laugh.",
+    summary: "Designer (# YOE), 9AM-5PM EST",
+    sectionLabel0: "Strengths",
+    sectionLabel1: "Weaknesses",
+    sectionLabel2: "Communication Preferences",
+    sectionContent0: "Making happy mistakes",
+    sectionContent1: "Nothing",
+    sectionContent2: "bobross@happymistakes.com",
+    sliderLabelMin: "Introvert",
+    sliderLabelMax: "Extrovert",
+    sliderValue: 80,
+  };
+
+  const TempFive = {
+    name: "Bob Ross",
+    city: "NYC",
+    tagline: "I love to paint, and I love to laugh.",
+    summary: "Designer (# YOE), 9AM-5PM EST",
+    sectionLabel0: "Strengths",
+    sectionLabel1: "Weaknesses",
+    sectionLabel2: "Communication Preferences",
+    sectionContent0: "Making happy mistakes",
+    sectionContent1: "Nothing",
+    sectionContent2: "bobross@happymistakes.com",
+    sliderLabelMin: "Introvert",
+    sliderLabelMax: "Extrovert",
+    sliderValue: 80,
+  };
+
+  const allCards = [TempOne, TempTwo, TempThree, TempFour, TempFive];
   return (
     <div>
       {" "}
-      <h1>this is a deck with id={id}</h1>{" "}
+      <p>this is a deck with id={id}</p>{" "}
+      <div class="deck-list">
+      {allCards.map((tempType) => (
+        <DisplayCard tempArray={tempType}></DisplayCard>
+      ))}
+      </div>
       <NavLink to={`${id}/add`}>Add Card</NavLink>
     </div>
   );
