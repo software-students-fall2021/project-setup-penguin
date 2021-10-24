f3fdab23f78f1f9ac8def43b2f1c7377e9902892
cindykimxp <cindykim100@gmail.com>
2021-10-23 21:53:55 -0400

    finished with basic css styling

--- front-end/src/views/DeckView.jsx
+++ front-end/src/views/DeckView.jsx
@@ -6,10 +6,13 @@ function DeckView() {
   let { id } = useParams();
   console.log({ id });
 
+  const title = "SWE";
+  const subtitle = "Team for SWE Project, Fall 2021"
+
   const TempOne = {
     name: "Bob Ross",
     city: "NYC",
-    tagline: "I love to paint, and I love to laugh.",
+    tagline: "I love to paint, and laugh.",
     summary: "Designer (# YOE), 9AM-5PM EST",
     sectionLabel0: "Strengths",
     sectionLabel1: "Weaknesses",
@@ -23,80 +26,86 @@ function DeckView() {
   };
 
   const TempTwo = {
-    name: "Bob Ross",
+    name: "Pikachu",
     city: "NYC",
-    tagline: "I love to paint, and I love to laugh.",
-    summary: "Designer (# YOE), 9AM-5PM EST",
+    tagline: "Pika!!!!",
+    summary: "Pokemon(# YOE), 10AM-11PM EST",
     sectionLabel0: "Strengths",
     sectionLabel1: "Weaknesses",
     sectionLabel2: "Communication Preferences",
-    sectionContent0: "Making happy mistakes",
-    sectionContent1: "Nothing",
-    sectionContent2: "bobross@happymistakes.com",
+    sectionContent0: "Pika Pika",
+    sectionContent1: "Chu",
+    sectionContent2: "pika@chu.com",
     sliderLabelMin: "Introvert",
     sliderLabelMax: "Extrovert",
-    sliderValue: 80,
+    sliderValue: 20,
   };
 
   const TempThree = {
-    name: "Bob Ross",
+    name: "Ariana Grande",
     city: "NYC",
-    tagline: "I love to paint, and I love to laugh.",
-    summary: "Designer (# YOE), 9AM-5PM EST",
+    tagline: "*whistle singing*",
+    summary: "Singer and Artist (# YOE), 6AM-11PM EST",
     sectionLabel0: "Strengths",
     sectionLabel1: "Weaknesses",
     sectionLabel2: "Communication Preferences",
-    sectionContent0: "Making happy mistakes",
-    sectionContent1: "Nothing",
-    sectionContent2: "bobross@happymistakes.com",
+    sectionContent0: "Singing",
+    sectionContent1: "Walking in sneakers",
+    sectionContent2: "arianatalent@gmail.com",
     sliderLabelMin: "Introvert",
     sliderLabelMax: "Extrovert",
-    sliderValue: 80,
+    sliderValue: 95,
   };
 
   const TempFour= {
-    name: "Bob Ross",
+    name: "Andrew Hamilton",
     city: "NYC",
-    tagline: "I love to paint, and I love to laugh.",
-    summary: "Designer (# YOE), 9AM-5PM EST",
+    tagline: "Billionaire, President of NYU",
+    summary: "Money-maker (# YOE), 24/7",
     sectionLabel0: "Strengths",
     sectionLabel1: "Weaknesses",
     sectionLabel2: "Communication Preferences",
-    sectionContent0: "Making happy mistakes",
-    sectionContent1: "Nothing",
-    sectionContent2: "bobross@happymistakes.com",
+    sectionContent0: "I make money",
+    sectionContent1: "I can't not make money",
+    sectionContent2: "andyfanmail@gmail.com",
     sliderLabelMin: "Introvert",
     sliderLabelMax: "Extrovert",
-    sliderValue: 80,
+    sliderValue: 50,
   };
 
   const TempFive = {
-    name: "Bob Ross",
-    city: "NYC",
-    tagline: "I love to paint, and I love to laugh.",
-    summary: "Designer (# YOE), 9AM-5PM EST",
+    name: "Jane Doe",
+    city: "Unknown",
+    tagline: "You don't know me",
+    summary: "Missing (# YOE), 9AM-5PM EST",
     sectionLabel0: "Strengths",
     sectionLabel1: "Weaknesses",
     sectionLabel2: "Communication Preferences",
-    sectionContent0: "Making happy mistakes",
-    sectionContent1: "Nothing",
-    sectionContent2: "bobross@happymistakes.com",
+    sectionContent0: "I can disappear",
+    sectionContent1: "You'll never get to know me",
+    sectionContent2: "?",
     sliderLabelMin: "Introvert",
     sliderLabelMax: "Extrovert",
-    sliderValue: 80,
+    sliderValue: 10,
   };
 
   const allCards = [TempOne, TempTwo, TempThree, TempFour, TempFive];
   return (
     <div>
       {" "}
-      <p>this is a deck with id={id}</p>{" "}
+      <p>(this is a deck with id={id})</p>{" "}
+      <div class="header">
+        <div class="title">{title}</div>
+        <div class="subtitle">{subtitle}</div>
+      </div>
       <div class="deck-list">
       {allCards.map((tempType) => (
         <DisplayCard tempArray={tempType}></DisplayCard>
       ))}
       </div>
-      <NavLink to={`${id}/add`}>Add Card</NavLink>
+      <div class="add-card">
+        <NavLink to={`${id}/add`}>Add Card</NavLink>
+      </div>
     </div>
   );
 }
