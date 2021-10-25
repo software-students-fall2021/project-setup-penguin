f26583b591331f4bb28b8e370902a0d93440478d
Janet <jh6248@nyu.edu>
2021-10-25 15:09:05 -0400

    add brief documentation for Button component

--- front-end/src/common/components/Button.jsx
+++ front-end/src/common/components/Button.jsx
@@ -1,6 +1,12 @@
 import "./Button.css";
 import { Link } from "react-router-dom";
 
+/**
+ * you should pass in either onClick or linkTo
+ * @param linkTo object/string passed into <Link> component – use to change routes
+ * @param onClick function called on button click – use to trigger same-page behavior (modal)
+ * @returns
+ */
 function Button({ btnText, onClick, linkTo, icon }) {
   const btn = (
     <button className="Button" onClick={onClick}>
