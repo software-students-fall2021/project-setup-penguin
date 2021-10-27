656adb0903ce25dcf5acd28054427b73a62e8d19
zhangrmatthew <mrz262@stern.nyu.edu>
2021-10-26 20:59:52 -0400

    Change howto buttons to use <Link/>

--- front-end/src/views/home/Home.jsx
+++ front-end/src/views/home/Home.jsx
@@ -1,28 +1,25 @@
 import card1 from "../../media/howto_card.png";
 import card2 from "../../media/howto_card2.png";
-
+import { Link } from "react-router-dom";
 import "./Home.css";
 
-function Home({ goToInviteLink, goToGetStarted }) {
+function Home() {
   return (
     <div className="Home">
       <div className="Home__header">How it works</div>
       <div className="Home__body">
-        {" "}
         Get to know your teammates through Pokémon-inspired trading cards
         highlighting superpowers, weaknesses, communication preferences, and
         more to have a more efficient and enjoyable working experience!
       </div>
 
       <div className="Home__list">
-        {" "}
         1. Create a card template for your team
       </div>
 
       <img className="Home_imgs" src={card1} alt="HomeCard1" />
 
       <div className="Home__list">
-        {" "}
         2. Fill out the template with your info
       </div>
 
@@ -30,18 +27,16 @@ function Home({ goToInviteLink, goToGetStarted }) {
 
       <div className="Home__list"> 3. Invite your teammates to join</div>
 
-      <div className="Invite__button" onClick={goToInviteLink}>
-        {" "}
-        Invite Link
+      <div className="Home_link" >
+        <Link to="/"> Invite Link</Link>
       </div>
 
       <div className="Home__list"> 4. Get to know 'em all'</div>
 
       <img className="Home_imgs" src={card2} alt="HomeCard2" />
 
-      <div className="Invite__button" onClick={goToGetStarted}>
-        {" "}
-        Get Started
+      <div className="Home_link" >
+        <Link to="/createdeck">Get Started</Link>
       </div>
     </div>
   );
