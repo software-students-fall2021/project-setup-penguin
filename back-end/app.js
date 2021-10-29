ba03552cd7abdc294986a4d91a677eab1115be9e
Kevin Duan <58224004+kdduan@users.noreply.github.com>
2021-10-29 13:38:26 -0400

    added /deck post endpoint and connected FinishDeckSetup (#97)
    * added /deck post endpoint and connected FinishDeckSetup
    
    * added /deck post endpoint and connected FinishDeckSetup

--- back-end/app.js
+++ back-end/app.js
@@ -3,9 +3,11 @@ const app = express();
 
 // middleware
 const morgan = require("morgan");
+const cors = require("cors");
 
 // use the morgan middleware to log all incoming http requests
 app.use(morgan("dev")); // morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(cors()); // prevents requests from being blocked by CORS
 
 // use express's builtin body-parser middleware to parse any data included in a request
 app.use(express.json()); // decode JSON-formatted incoming POST data
@@ -14,6 +16,28 @@ app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming 
 // route for HTTP GET requests to root endpoint
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// POST endpoint used to create a new deck
+app.post("/deck", (req, res) => {
+  const { userId, deckName, deckDescription, cardTemplate } = req.body;
+
+  // will need to write these values to the deck db later
+  console.log("userId:", userId);
+  console.log("deckName:", deckName);
+  console.log("deckDescription:", deckDescription);
+  console.log("cardTemplate:", cardTemplate);
+
+  // will need to add the cardData to the user later
+  const cardData = {
+    userId,
+    ...cardTemplate,
+  };
+  console.log("cardData:", cardData);
+
+  res.json({
+    deckId: 1, // dummy deckId
+  });
 });
 
 module.exports = app;
