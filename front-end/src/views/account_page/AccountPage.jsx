bb96e0f6cc052ec4d7736bdd274b0526c4b08b17
jharsch2401 <60206755+jharsch2401@users.noreply.github.com>
2021-10-28 21:17:48 -0400

    update account page to use mock data (#86)

--- front-end/src/views/account_page/AccountPage.jsx
+++ front-end/src/views/account_page/AccountPage.jsx
@@ -2,9 +2,11 @@ import "./AccountPage.css";
 import DisplayCard from "../../common/DisplayCard";
 import React, { useState } from 'react';
 import { useParams, NavLink } from "react-router-dom";
-import {FORM_DEFAULT_PLACEHOLDERS} from "../../common/constants";
-
-const title = "SWE";
+import {
+    TEST_CARDS_ARRAY,
+  } from "../../common/constants";
+import axios from "axios";
+import { useEffect } from "react";
 
 function AccountPage() { 
     const states = ["Active", "inactive"]; //array for defining classes of button states
@@ -13,6 +15,27 @@ function AccountPage() {
     let pageContent;
     const [deckActive, setDeckActive] = useState(0); 
     //0 = deck view, 1 = card view (for easy class switching for styling and content display using states array)
+
+    const [templateArray, setTemplateData] = useState([]);
+    const [deckTitle, setDeckName] = useState();
+    const [deckSubtitle, setDeckDescription] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`https://my.api.mockaroo.com/deck/123?key=d5aa71f0`)
+      .then((response) => {
+        console.log("data", response.data.cards);
+        setTemplateData(response.data.cards);
+        setDeckName(response.data.deckName);
+        setDeckDescription(response.data.deckDescription);
+      })
+      .catch((err) => {
+        console.log("!!", err);
+        setTemplateData(TEST_CARDS_ARRAY);
+        setDeckName("SWE");
+        setDeckDescription("Team for SWE Project, Fall 2021");
+      })
+  }, []);
 
 //onClick functions for changing state for display
     const activateDeckView = () =>{
@@ -27,11 +50,16 @@ function AccountPage() {
   TO DO: update with actual card and deck display once DeckView and DisplayCard are updated,
     will use arrays to display multiple cards/decks when user functionality is further along */
     if(deckActive === 0){
-        pageContent = <><NavLink to={`deck/${id}`} className="title">{title}</NavLink></>;
+        pageContent = <><NavLink to={`deck/${id}`} className="title">{deckTitle}</NavLink>
+        <h2>{deckSubtitle}</h2></>;
         
     }
     else{
-        pageContent = <><h3 className="subtitle">Card 1</h3><DisplayCard tempArray={FORM_DEFAULT_PLACEHOLDERS}/></>;
+        pageContent = <><div class="deck-list">
+        {templateArray.map((tempType) => (
+          <DisplayCard tempArray={tempType}></DisplayCard>
+        ))}
+      </div></>;
     }
     return(
         <div className="container">
