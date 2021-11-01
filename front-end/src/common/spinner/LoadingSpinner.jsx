aa22069066310b2687760f55330170c859dee88e
Kevin Duan <58224004+kdduan@users.noreply.github.com>
2021-11-01 18:10:23 -0400

    added LoadingSpinner component (#101)
    * added basic spinner to DeckView
    
    * created common LoadingSpinner component

---
+++ front-end/src/common/spinner/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+function LoadingSpinner() {
+  return (
+    <div
+      class="spinner-border text-primary"
+      style={{ width: "4rem", height: "4rem" }}
+      role="status"
+    ></div>
+  );
+}
+
+export default LoadingSpinner;
