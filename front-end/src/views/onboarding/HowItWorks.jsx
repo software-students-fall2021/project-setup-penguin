2466c290a3629bbbceda8283ba78d9d3c65e3937
zhangrmatthew <mrz262@ster.nyu.edu>
2021-11-10 15:02:54 -0500

    Update design and text

--- front-end/src/views/onboarding/HowItWorks.jsx
+++ front-end/src/views/onboarding/HowItWorks.jsx
@@ -1,10 +1,42 @@
-import pokedex from "../../assets/pokedex.gif";
-import team from "../../assets/team.png";
-import card from "../../assets/card.png";
-import { Button } from "../../common";
-import * as Icon from "react-bootstrap-icons";
-import "./HowItWorks.css";
+import pokedex from '../../assets/pokedex.gif';
+import team from '../../assets/team.png';
+import card from '../../assets/card.png';
+import { Button } from '../../common';
+import * as Icon from 'react-bootstrap-icons';
+import './HowItWorks.css';
 
+const howToSteps = [
+  {
+    header: 'CREATE',
+    subHeader: 'A DECK',
+    content:
+      'Set up a template card for your team. Decide the qualities you want to share and learn about your teammates!',
+    button: {
+      text: 'Create a deck',
+      link: 'www.google.com',
+    },
+  },
+  {
+    header: 'SHARE',
+    subHeader: 'YOUR DECK',
+    content:
+      'Share a link with your teammates so that they can add their own cards to your deck.',
+    button: {
+      text: '',
+      link: '',
+    },
+  },
+  {
+    header: 'MEET',
+    subHeader: 'THE TEAM',
+    content:
+      "Look through a deck to find our more about your teammates. You can edit your card, or even edit and delete deck details. Get to know e'm all!",
+    button: {
+      text: '',
+      link: '',
+    },
+  },
+];
 // TODO: arrange horizontally ?
 function HowItWorks() {
   return (
@@ -17,20 +49,23 @@ function HowItWorks() {
           icon={<Icon.ArrowRight />}
         />
       </div>
-      <p className="HowItWorks__body">
-        Get to know your teammates through Pokémon-inspired trading cards
-        highlighting superpowers, weaknesses, communication preferences, and
-        more to have a more efficient and enjoyable working experience!
-      </p>
-      <p>1. Set up a template card for your team by creating your own card</p>
-      <img className="HowItWorks_imgs" src={card} alt="Card" />
-      <p>
-        2. Share a link to with your teammates so that they can add their own
-        cards to your deck
-      </p>
-      <img className="HowItWorks_imgs" src={team} alt="Team" />
-      <p> 3. Get to know 'em all!</p>
-      <img className="HowItWorks_imgs" src={pokedex} alt="Pokedex" />
+      <div className="HowItWorks__body">
+        {howToSteps.map((item, index) => (
+          <div className="HowItWorks__row">
+            <h2 className="HowItWorks__rowIndex">{index + 1}</h2>
+            <div className="HowItWorks__rowHeaderContainer">
+              <h2 className="HowItWorks__rowHeader">{item.header}</h2>
+              <h4 className="HowItWorks__rowSubHeader">{item.subHeader}</h4>
+            </div>
+            <div className="HowItWorks__rowContent">{item.content}</div>
+            {item.button?.text && item.button.link && (
+              <button className="HowItWorks__rowButton">
+                {item.button.text}
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
