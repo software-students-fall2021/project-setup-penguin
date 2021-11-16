2273c0299eb9a07079fcd127e47ec6a1a264bb6e
Janet Huang <janet.huang@stern.nyu.edu>
2021-11-15 19:50:23 -0500

    new branch w/ express-validator (#162)

--- back-end/controllers/deckController.js
+++ back-end/controllers/deckController.js
@@ -1,5 +1,6 @@
 const fs = require("fs").promises;
 const shortid = require("shortid");
+const { validationResult } = require("express-validator");
 
 const db = require("../db.js");
 const User = require("../models/user");
@@ -56,6 +57,11 @@ const getDeck = (req, res, next) => {
 };
 
 const createDeck = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const { userId, deckName, deckDescription, cardText } = req.body;
 
   const cardTemplate = JSON.parse(cardText);
@@ -112,6 +118,11 @@ const createDeck = async (req, res, next) => {
 };
 
 const updateDeck = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const deckId = req.params.deckId;
   const { deckName, deckDescription } = req.body;
 
--- back-end/routes/cardRoutes.js
+++ back-end/routes/cardRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const cardController = require("../controllers/cardController");
+const upload = require("../multerConfig");
+const { body } = require("express-validator");
 
-router.post("/", cardController.createCard);
+router.post(
+  "/",
+  body("deckId").notEmpty(),
+  upload.single("cardImage"),
+  cardController.createCard
+);
 
 //  axios.delete('baseUrl/card', { data: {userId, deckId} })
 router.delete("/:cardId", cardController.deleteCard);
