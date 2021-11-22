376c61fbe4785ec39350735b291dea46a3bcf75d
zhangrmatthew <mrz262@stern.nyu.edu>
2021-11-22 17:27:13 -0500

    Update delete, get, getAccount, patch userRoutes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { body } = require("express-validator");
@@ -14,13 +15,31 @@ router.post(
   userController.createUser
 );
 
-router.delete("/:userId", userController.deleteUser);
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  userController.deleteUser
+);
 
-router.get("/", authenticate, userController.getUser);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  userController.getUser
+);
+
+router.get(
+  "/account",
+  passport.authenticate("jwt", { session: false }),
+  userController.getUserAccount
+);
 
 // TODO: update this to align with get request (get id from req.user not req.params)
 // TODO: incorporate express-validator
-router.patch("/:userId", userController.updateUser);
+router.patch(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  userController.updateUser
+);
 
 router.post(
   "/login",
