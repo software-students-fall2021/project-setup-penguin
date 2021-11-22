51edaa10b4155f0f735c2e71d9e2ab7efb96e8eb
cindykimxp <cindykim100@gmail.com>
2021-11-21 19:18:58 -0500

    home-page minor css changes

--- front-end/src/views/onboarding/Home.jsx
+++ front-end/src/views/onboarding/Home.jsx
@@ -1,6 +1,6 @@
 import card from "../../assets/card.png";
 import "./Home.css";
-import { Button, useViewport } from "../../common";
+import { Button, DarkButton, useViewport } from "../../common";
 
 function Home() {
   const { width } = useViewport();
@@ -9,17 +9,19 @@ function Home() {
   return (
     <div className={isMobile ? "Home__mobile" : "Home"}>
       <div className={isMobile ? "Home__section--mobile" : "Home__section"}>
-        <h1>Get to know your team in the best way possible</h1>
+        <h1 className="Home__main-title">Get to know your team in the best way possible</h1>
         <p>
           We make team-building more easy and fun through Pokémon-inspired cards
           for people.
         </p>
-        <span className="Home__btn">
-          <Button btnText="How it works" linkTo="/howitworks" />
-        </span>
-        <span className="Home__btn">
-          <Button btnText="Get started" linkTo="/getstarted" />
-        </span>
+        <div className="Home__btn-group">
+          <span className="Home__btn">
+            <Button btnText="How it works" linkTo="/howitworks" />
+          </span>
+          <span className="Home__btn">
+            <DarkButton btnText="Get started" linkTo="/getstarted" />
+          </span>
+        </div>
       </div>
       <img
         className={isMobile ? "Home__img--mobile" : "Home__img"}
