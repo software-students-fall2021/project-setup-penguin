0cd6d253ccb1b28685d0e73c9abea465eecdf08b
zhangrmatthew <mrz262@stern.nyu.edu>
2021-11-22 17:24:48 -0500

    update delete get, update and login controllers

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -45,45 +45,42 @@ const createUser = async (req, res, next) => {
   res.json({ success: true, email: user.email, token: token });
 };
 
-const deleteUser = (req, res, next) => {
-  const userId = req.params.userId;
-  fs.readFile("database.json")
-    .then((data) => {
-      try {
-        const jsonData = JSON.parse(data);
+const deleteUser = async (req, res, next) => {
+  const userId = req.user._id;
+  const userData = await User.findOne({ _id: userId });
+  const userCardIds = userData.cards;
 
-        // update user document
-        if (!(userId in jsonData.users)) {
-          next({ message: "User does not exist" });
-        }
+  //remove all cards from decks
+  userCardIds.forEach(async (cardId) => {
+    const card = await Card.findOne({ _id: cardId });
 
-        // delete cards associated with user
-        jsonData.users[userId].cards.forEach((card) => {
-          if (card && card in jsonData.cards) {
-            const deck = jsonData.cards[card].deckId;
-            const deckArr = jsonData.decks[deck].cards;
-            for (let i = 0; i < deckArr.length; i++) {
-              if (deckArr[i] === card) {
-                deckArr.splice(i, 1);
-                break;
-              }
-            }
-            delete jsonData.cards[card];
-          }
-        });
+    const deck = await Deck.findOne({ _id: card.deckId });
 
-        // delete user
-        delete jsonData.users[userId];
+    // delete card document
+    await Card.deleteOne({ _id: cardId }).catch((err) => next(err));
 
-        const jsonString = JSON.stringify(jsonData, null, 2);
-        fs.writeFile("database.json", jsonString)
-          .then(() => res.json({ userId }))
-          .catch((err) => next(err));
-      } catch (err) {
-        next(err);
-      }
-    })
-    .catch((err) => next(err));
+    // delete cardId from deck object
+    deck.cards = deck.cards.filter(
+      (currCardId) => currCardId.toString() != cardId
+    );
+    deck.save();
+  });
+
+  //make all decks set to the user null
+  const decks = await Deck.updateMany(
+    { ownerId: userId },
+    { $set: { ownerId: "619bd8f22314e7c3a4e5efb6" } }
+  );
+
+  //Delete user
+  try {
+    User.deleteOne({ _id: userId }).then(() => {
+      res.status(200);
+      res.json({ message: "User successfully deleted" });
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const getUser = async (req, res, next) => {
@@ -127,36 +124,64 @@ const getUser = async (req, res, next) => {
   res.send(userCardsWithDeckData);
 };
 
+const getUserAccount = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    const userData = await User.findOne({ _id: userId });
+
+    console.log(userData);
+    res.status(200);
+    res.json({ name: userData.name, email: userData.email });
+  } catch (error) {
+    res.status(200);
+  }
+};
+
 const updateUser = async (req, res, next) => {
-  const userId = req.params.userId;
+  const userId = req.user._id;
   const { email, password, name } = req.body;
 
-  // check if User already exists
-  User.countDocuments({ _id: userId }, function (err, count) {
-    if (count == 0) {
-      throw "User does not exist";
-    }
-  }).catch((err) => {
-    next(err);
+  //check if User does not exists
+  const count = await User.countDocuments({ _id: userId });
+  if (count === 0) {
+    throw "User does not exist";
+  }
+
+  //find relevant info - don't allow updates if conflicting email present
+  const prevInfo = await User.find({ _id: userId }).select({
+    _id: 0,
+    email: 1,
   });
 
-  await User.findOneAndUpdate({ _id: userId }, { name, email, password }).catch(
-    (err) => {
-      next(err);
+  if (!email || !password || !name) throw "Empty field";
+
+  if (prevInfo[0].email !== email) {
+    const conflict = await User.countDocuments({ email: email });
+    console.log(conflict);
+    if (conflict >= 1) {
+      throw "User email conflict";
     }
-  );
+  }
+
+  bcrypt.hash(password, saltRounds, async function (err, hash) {
+    await User.findOneAndUpdate({ _id: userId }, { name, email, hash })
+      .exec()
+      .catch((err) => {
+        next(err);
+      });
+  });
   res.json({ userId });
 };
 
 const loginUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const messages = errors.array().map((error) => error.msg);
-    return res.status(400).json({ messages });
-  }
-
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    res
+      .status(401)
+      .json({ success: false, message: "no email or password provided" });
+  }
   const user = await User.findOne({ email: email });
   if (!user) {
     res
@@ -166,7 +191,6 @@ const loginUser = async (req, res, next) => {
   const match = await bcrypt.compare(password, user.password).catch((err) => {
     next(err);
   });
-
   if (match) {
     const payload = { id: user._id };
     const token = jwt.sign(payload, jwtOptions.secretOrKey);
@@ -174,7 +198,7 @@ const loginUser = async (req, res, next) => {
   } else {
     res
       .status(401)
-      .json({ success: false, messages: [`passwords did not match`] });
+      .json({ success: false, message: "passwords did not match" });
   }
 };
 
@@ -184,4 +208,5 @@ module.exports = {
   deleteUser,
   updateUser,
   loginUser,
+  getUserAccount,
 };
